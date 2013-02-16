0a97f288bc558844a6222d183d929e4831116ac7
dplus86 <dplus86@mail.ru>
2013-02-16 22:05:57 +0300

    asdf

--- JavaOnline/src/Lesson_02/SecondClas.java
+++ JavaOnline/src/Lesson_02/SecondClas.java
@@ -8,7 +8,7 @@ public class SecondClas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is syn");
-
+		System.out.println("This add in fujitsu");
 	}
 
 }
